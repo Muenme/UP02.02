3984aefbf2b2d465340714c4ff73f05cac7e312d
«Мэри <muenme.world@gmail.com>
2026-05-21 20:21:50 +0300

    Полный merge

--- app/src/main/java/com/example/inheck/screen/EditBuy.kt
+++ app/src/main/java/com/example/inheck/screen/EditBuy.kt
@@ -5,11 +5,12 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.example.inheck.data.entity.Buy
 
 @Composable
 fun EditBuy(
     onBack: () -> Unit,
-    toEditProduct: () -> Unit
+    toEditProduct: () -> Unit,
 )
 {
     Column {
